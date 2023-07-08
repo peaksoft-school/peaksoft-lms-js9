1c1b220fd9ee83792b269103e58c275cdd0f2de9
Aimona Dunaeva <aimonadunaeva@gmail.com>
2023-07-08 13:43:46 +0600

    redone input

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -2,31 +2,37 @@ import React, { forwardRef } from 'react'
 import { TextField } from '@mui/material'
 import styled from '@emotion/styled'
 
-export const Input = forwardRef((props, ref) => {
-   const { placeholder, onChange, type, value, ...restprops } = props
-   return (
-      <div>
-         <TextInput
-            value={value}
-            onChange={onChange}
-            variant="outlined"
-            fullWidth
-            type={type}
-            placeholder={placeholder}
-            ref={ref}
-            {...restprops}
-         />
-      </div>
-   )
-})
+export const Input = forwardRef(
+   ({ placeholder, onChange, type, value, error, ...rest }, ref) => {
+      return (
+         <div>
+            <TextInput
+               value={value}
+               onChange={onChange}
+               variant="outlined"
+               type={type}
+               placeholder={placeholder}
+               ref={ref}
+               error={Boolean(error)}
+               {...rest}
+            />
+         </div>
+      )
+   }
+)
 
-const TextInput = styled(TextField)((props) => ({
-   input: {
-      color: props.error ? '#f00' : '#757575',
-   },
-   'MuiOutlinedInput-root': {
-      '&:hover fieldset': {
-         borderColor: props.error ? '#f00' : '#3a10e5',
-      },
-   },
-}))
+const TextInput = styled(TextField)`
+   fieldset {
+      border-radius: 10px;
+      padding: 10px 8px 10px 18px;
+   }
+   /* width: 30%; */
+   input:focus {
+      border-radius: 10px;
+      border: 2px solid #1f6ed4;
+   }
+   input:invalid {
+      border-radius: 10px;
+      border: 2px solid #c91e1e;
+   }
+`
