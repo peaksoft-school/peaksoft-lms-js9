f4424566a04a3ad01677ab9663a30cc139d647aa
Nurislam <uchdobo04.com>
2023-07-11 19:16:43 +0600

    changed meatball codes

--- src/components/meatBall/MeatBall.jsx
+++ src/components/meatBall/MeatBall.jsx
@@ -1,42 +1,90 @@
 import * as React from 'react'
-import Paper from '@mui/material/Paper'
-import MenuList from '@mui/material/MenuList'
+import { styled, alpha } from '@mui/material/styles'
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
-import ListItemText from '@mui/material/ListItemText'
-import { styled } from '@mui/material'
-import { MeatBallIcon } from '../../assets/icons'
 
-export default function MeatBall({ menuItems, handleClick, open, onClick }) {
-   const titleHandler = (event) => {
-      onClick(event)
+export default function MeatBall({ menuItems, onClick }) {
+   const [anchorEl, setAnchorEl] = React.useState(null)
+   const open = Boolean(anchorEl)
+   const handleClick = (event) => {
+      setAnchorEl(event.currentTarget)
    }
+   const handleClose = () => {
+      setAnchorEl(null)
+   }
+
    return (
       <div>
-         {open ? (
-            <Paper sx={{ width: '10.30rem', height: 'auto' }}>
-               <MenuList>
-                  {menuItems.map((item) => (
-                     <MenuItem key={item.id}>
-                        <img src={item.img} alt="not found" />
-                        <LisItemTexts onClick={() => titleHandler(item.title)}>
-                           {item.title}
-                        </LisItemTexts>
-                     </MenuItem>
-                  ))}
-               </MenuList>
-            </Paper>
-         ) : (
-            <button type="button" onClick={handleClick}>
-               <MeatBallIcon />
-            </button>
-         )}
+         <Button
+            id="demo-customized-button"
+            aria-controls={open ? 'demo-customized-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            variant="contained"
+            disableElevation
+            onClick={handleClick}
+         >
+            Options
+         </Button>
+         <StyledMenu
+            id="demo-customized-menu"
+            MenuListProps={{
+               'aria-labelledby': 'demo-customized-button',
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+         >
+            {menuItems.map((item) => (
+               <MenuItem key={item.id} onClick={() => onClick(item.id)}>
+                  <img src={item.img} alt="not found" />
+                  {item.title}
+               </MenuItem>
+            ))}
+         </StyledMenu>
       </div>
    )
 }
-
-const LisItemTexts = styled(ListItemText)(() => ({
-   fontFamily: 'Open Sans',
-   fontSize: '0.875rem',
-   fontWeight: 400,
-   lineHeight: '1.5rem',
+const StyledMenu = styled((props) => (
+   <Menu
+      elevation={0}
+      anchorOrigin={{
+         vertical: 'bottom',
+         horizontal: 'right',
+      }}
+      transformOrigin={{
+         vertical: 'top',
+         horizontal: 'right',
+      }}
+      {...props}
+   />
+))(({ theme }) => ({
+   '& .MuiPaper-root': {
+      borderRadius: 6,
+      marginTop: theme.spacing(1),
+      minWidth: 180,
+      color:
+         theme.palette.mode === 'light'
+            ? 'rgb(55, 65, 81)'
+            : theme.palette.grey[300],
+      boxShadow:
+         'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+      '& .MuiMenu-list': {
+         padding: '4px 0',
+      },
+      '& .MuiMenuItem-root': {
+         '& .MuiSvgIcon-root': {
+            fontSize: 18,
+            color: theme.palette.text.secondary,
+            marginRight: theme.spacing(1.5),
+         },
+         '&:active': {
+            backgroundColor: alpha(
+               theme.palette.primary.main,
+               theme.palette.action.selectedOpacity
+            ),
+         },
+      },
+   },
 }))
