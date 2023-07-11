601bcede6ed30e4543a09c286a7df13024ef95b6
nurzhigitabdymanapov <abdymanapovn01@gmail.com>
2023-07-11 19:34:03 +0600

    fixed reusable modal

--- src/components/UI/modal/Modal.jsx
+++ src/components/UI/modal/Modal.jsx
@@ -1,34 +1,56 @@
-import { Box, Modal, styled } from '@mui/material'
-import React from 'react'
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import { Modal as ModalUi, styled } from '@mui/material'
 
-export const ModalUi = ({ children, minHeight, width, ...rest }) => {
+export const Modal = ({ minHeight, title, children }) => {
+   const [open, setOpen] = React.useState(false)
+   const handleOpen = () => setOpen(true)
+   const handleClose = () => setOpen(false)
+
    return (
-      <MyModal onClose={handleClose} open={open}>
-         <ModalStyle minHeight={minHeight} width={width} {...rest}>
-            {children}
-         </ModalStyle>
-      </MyModal>
+      <div>
+         <Button onClick={handleOpen}>Open modal</Button>
+         <ModalUi open={open} onClose={handleClose}>
+            <StyledModal minHeight={minHeight}>
+               <Header>
+                  <p>{title}</p>
+               </Header>
+               <Content>{children}</Content>
+            </StyledModal>
+         </ModalUi>
+      </div>
    )
 }
-const ModalStyle = styled(Box)(({ width, minHeight, ...rest }) => ({
-   position: 'fixed',
-   left: '50%',
-   backgroundColor: 'white',
-   borderRadius: ' 0.625rem',
-   zIndex: '30',
-   width: width || rest,
-   minHeight: minHeight || rest,
-   top: '40%',
-   transform: 'translate(-50%, -50%)',
-   border: 'none',
-}))
-const MyModal = styled(Modal)(() => ({
-   position: 'absolute',
-   top: '0',
-   left: '0',
+
+const Header = styled('div')(() => ({
    width: '100%',
-   height: 'auto',
-   backgroundColor: 'rgba(239, 230, 230, 0.464)',
-   backdropFilter: 'blur(2px)',
-   zIndex: '998',
+   height: ' 4.25rem',
+   background: 'var(--button, #3772FF)',
+   borderRadius: ' 0.625rem 0.625rem 0rem 0rem',
+   padding: '0',
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'center',
+   p: {
+      color: '#FFF',
+      textAlign: 'center',
+      fontSize: '1.25rem',
+      fontWeight: '400',
+   },
+}))
+
+const StyledModal = styled(Box)(() => ({
+   position: 'absolute',
+   top: '50%',
+   left: '50%',
+   transform: 'translate(-50%, -50%)',
+   width: '33.8125rem',
+   backgroundColor: '#fff',
+   border: 'none',
+   minHeight: '13.1875rem',
+   borderRadius: '0.625rem',
+}))
+const Content = styled('div')(() => ({
+   padding: '1rem 1.56rem 1.56rem 1.56rem',
 }))
