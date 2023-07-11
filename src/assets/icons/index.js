d4de0bb419e6c3464e663986f3c4b8998b491e04
Nurislam <uchdobo04.com>
2023-07-11 22:40:08 +0600

    added icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,91 @@
+import { ReactComponent as AddDownIcon } from './addDownIcon.svg'
+import { ReactComponent as AddMoreIcon } from './addMoreIcon.svg'
+import { ReactComponent as AddTextIcon } from './addTextTIcon.svg'
+import { ReactComponent as AddToButtonIcon } from './addToButtonIcon.svg'
+import { ReactComponent as AppointIcon } from './appointZakIcon.svg'
+import { ReactComponent as BoldTextIcon } from './boldTextIcon.svg'
+import { ReactComponent as CalendarIcon } from './calendarIcon.svg'
+import { ReactComponent as CancelIcon } from './cancelIcon.svg'
+import { ReactComponent as CheckIsEmptyIcon } from './checkIsEmptyIcon.svg'
+import { ReactComponent as CheckMarkedIcon } from './checkMarkedIcon.svg'
+import { ReactComponent as CheckedFullBlackIcon } from './chequeFullBlackIcon.svg'
+import { ReactComponent as ClosedEyePassIcon } from './closedEyePassIcon.svg'
+import { ReactComponent as CodeTagIcon } from './codeTag.svg'
+import { ReactComponent as CoursesBlueIcon } from './coursesBlueIcon.svg'
+import { ReactComponent as CoursesIcon } from './coursesIcon.svg'
+import { ReactComponent as ItalicsIcon } from './italicsIcon.svg'
+import { ReactComponent as DeleteIcon } from './deleteIcon.svg'
+import { ReactComponent as DropDownIcon } from './dropDownIcon.svg'
+import { ReactComponent as DuplicateIcon } from './duplicateIcon.svg'
+import { ReactComponent as EditIcon } from './editIcon.svg'
+import { ReactComponent as ElipsListIcon } from './elipsListLi.svg'
+import { ReactComponent as ErrorBlackIcon } from './errorBlackIcon.svg'
+import { ReactComponent as JobOrFileIcon } from './exerciseOrFileIcon.svg'
+import { ReactComponent as FontSizesIcon } from './fontSizesIcon.svg'
+import { ReactComponent as GroupsBlueIcon } from './groupsBlueIcon.svg'
+import { ReactComponent as GroupsIcon } from './groupsTwoIcon.svg'
+import { ReactComponent as ImportIcon } from './import.svg'
+import { ReactComponent as LazyLoadingIcon } from './lazyLoadingIcon.svg'
+import { ReactComponent as LinkIcon } from './linkIcon.svg'
+import { ReactComponent as ListNumberedIcon } from './listNumberedOlIcon.svg'
+import { ReactComponent as OpenEyePassIcon } from './openEyePassIcon.svg'
+import { ReactComponent as PhotoIcon } from './photoIcon.svg'
+import { ReactComponent as PresentationIcon } from './presentationIcon.svg'
+import { ReactComponent as RadioEmptyIcon } from './radioEmptyIcon.svg'
+import { ReactComponent as RadioMarkedIcon } from './radioMarkedIcon.svg'
+import { ReactComponent as StrikeoutTextIcon } from './strikeoutTextIcon.svg'
+import { ReactComponent as StudentIcon } from './studentOne.svg'
+import { ReactComponent as StudentsBlueIcon } from './studentsBlueIcon.svg'
+import { ReactComponent as StudentsIcon } from './studentsIcon.svg'
+import { ReactComponent as SuccessfulBlackIcon } from './successfulBlackIcon.svg'
+import { ReactComponent as TeachersBlueIcon } from './teachersBlueIcon.svg'
+import { ReactComponent as TeachersIcon } from './teachersIcon.svg'
+import { ReactComponent as VideoTutorialIcon } from './videoTutorialLesIcon.svg'
+import { ReactComponent as MeatBallIcon } from './frameCardIcon.svg'
+
+export {
+   AddDownIcon,
+   AddMoreIcon,
+   AddTextIcon,
+   AddToButtonIcon,
+   AppointIcon,
+   BoldTextIcon,
+   CalendarIcon,
+   CancelIcon,
+   CheckIsEmptyIcon,
+   CheckMarkedIcon,
+   CheckedFullBlackIcon,
+   ClosedEyePassIcon,
+   CodeTagIcon,
+   CoursesBlueIcon,
+   CoursesIcon,
+   ItalicsIcon,
+   DeleteIcon,
+   DropDownIcon,
+   DuplicateIcon,
+   EditIcon,
+   ElipsListIcon,
+   ErrorBlackIcon,
+   JobOrFileIcon,
+   FontSizesIcon,
+   GroupsBlueIcon,
+   GroupsIcon,
+   ImportIcon,
+   LazyLoadingIcon,
+   LinkIcon,
+   ListNumberedIcon,
+   OpenEyePassIcon,
+   PhotoIcon,
+   PresentationIcon,
+   RadioEmptyIcon,
+   RadioMarkedIcon,
+   StrikeoutTextIcon,
+   StudentIcon,
+   StudentsBlueIcon,
+   StudentsIcon,
+   SuccessfulBlackIcon,
+   TeachersBlueIcon,
+   TeachersIcon,
+   VideoTutorialIcon,
+   MeatBallIcon,
+}
