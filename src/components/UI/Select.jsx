582da37ac10efe55d9d906818567e6a10716eae7
Aimona Dunaeva <aimonadunaeva@gmail.com>
2023-07-12 17:43:46 +0600

    updated select

--- src/components/UI/Select.jsx
+++ src/components/UI/Select.jsx
@@ -10,7 +10,7 @@ const ITEM_PADDING_TOP = 8
 const MenuProps = {
    PaperProps: {
       style: {
-         maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+         maxHeight: ITEM_HEIGHT * 4.6 + ITEM_PADDING_TOP,
          width: 250,
       },
    },
