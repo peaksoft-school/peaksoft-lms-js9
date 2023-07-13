42f0c75a3d2e9a1c51b9905b1123bef5a62091f5
Aimona Dunaeva <aimonadunaeva@gmail.com>
2023-07-13 17:07:34 +0600

    backdrop finished

---
+++ src/components/Backdrop.jsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled'
+import { Box, Modal } from '@mui/material'
+
+export const Backdrop = ({ open, onClose, children }) => {
+   return (
+      <ModalStyled
+         open={open}
+         onClose={onClose}
+         aria-labelledby="modal-title"
+         aria-describedby="modal-description"
+      >
+         <Container>{children}</Container>
+      </ModalStyled>
+   )
+}
+
+const Container = styled(Box)(() => ({
+   backgroundColor: '#EFF0F4',
+   marginLeft: '1.25rem',
+   marginRight: '2.5rem',
+   marginTop: '4.69rem',
+}))
+
+const ModalStyled = styled(Modal)(() => ({
+   backgroundColor: '#8ebe7d',
+}))
