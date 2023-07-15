64a49d3f5df769327566164d2d7d55aaf5f1d132
nurzhigitabdymanapov <abdymanapovn01@gmail.com>
2023-07-15 18:09:12 +0600

    fixed some bags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { SignInPage } from './pages/SignInPage'
-
 function App() {
    return (
       <div>
-         {/* <h1>Peaksoft lms js9</h1> */}
-         <SignInPage />
+         <h1>Peaksoft lms js9</h1>
       </div>
    )
 }
