18d77e201bfdf4fc9c233f9817bdb1034045e8b8
Aimona Dunaeva <aimonadunaeva@gmail.com>
2023-07-19 18:40:53 +0600

    finished header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import { Header } from './components/Header'
-
 function App() {
    return (
       <div>
-         <Header
-            titleButton="+ Добавить урок"
-            titleTabs="Материалы"
-            titleTabs2="Студенты"
-            titlePage="Инструктор"
-            conditionButton="fkjb "
-         />
+         <h1>Peaksoft lms js9</h1>
       </div>
    )
 }
