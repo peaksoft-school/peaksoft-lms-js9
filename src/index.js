7ee7229254a4d55f2374c01a192904556692fbb1
Nurislam <nurisatakhanov@gmail.com>
2023-08-08 17:28:33 +0600

    added store and index

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material'
+import { Provider } from 'react-redux'
 import { theme } from './assets/styles/theme'
 import App from './App'
 import './index.css'
+import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
    <React.StrictMode>
       <ThemeProvider theme={theme}>
          <BrowserRouter>
-            <App />
+            <Provider store={store}>
+               <App />
+            </Provider>
          </BrowserRouter>
       </ThemeProvider>
    </React.StrictMode>
