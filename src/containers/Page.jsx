d217b38a9bb46b56c9ce50928bafeb528e805af9
Aimona Dunaeva <aimonadunaeva@gmail.com>
2023-09-18 23:59:07 +0600

    signInVideo

--- src/containers/Page.jsx
+++ src/containers/Page.jsx
@@ -1,18 +1,39 @@
-import { styled } from '@mui/material'
+import { Box, styled } from '@mui/material'
 import { useSelector } from 'react-redux'
 import React from 'react'
-import PageLogo from '../assets/icons/pageLogo.svg'
 import { Isloading } from '../components/UI/snackbar/Isloading'
+import kod from '../assets/video/kod.MP4'
+import logoPath from '../assets/icons/pageLogo.svg'
+
+const posterImage = require('../assets/video/kod.MP4') // Замените это на путь к изображению-постеру для видео
 
 export const Page = ({ children, title }) => {
    const { isLoading } = useSelector((state) => state.auth)
    return (
       <Container>
          <ContainerPageOne>
-            <ContainerLogo>
-               <img src={PageLogo} alt="PEAKSOFTLMSLOGO" />
-            </ContainerLogo>
+            <VideoContainer>
+               <video
+                  width="100%"
+                  height="100%"
+                  autoPlay
+                  loop
+                  muted
+                  poster={posterImage}
+                  style={{
+                     objectFit: 'cover',
+                     filter: 'brightness(50%)',
+                  }}
+                  controlsList="nodownload"
+               >
+                  <source src={kod} type="video/mp4" />
+               </video>
+               <LogoContainer>
+                  <Logo src={logoPath} alt="Логотип" />
+               </LogoContainer>
+            </VideoContainer>
          </ContainerPageOne>
+
          <ContainePageTwo>
             <ContainerTitles>{title}</ContainerTitles>
             <div>{children}</div>
@@ -26,22 +47,26 @@ const Container = styled('div')(() => ({
    width: '100%',
    height: '100vh',
    display: 'flex',
+   justifyContent: 'center', // Выравнивание по горизонтали
+   alignItems: 'center', // Выравнивание по вертикали
 }))
-const ContainerPageOne = styled('div')(() => ({
+
+const ContainerPageOne = styled(Box)(() => ({
    width: '45%',
    height: '100%',
-   background: 'var(--button, #3772FF)',
    display: 'flex',
    flexDirection: 'column',
    justifyContent: 'center',
    alignItems: 'center',
    gap: '5.63rem',
+   position: 'relative',
 }))
-const ContainerLogo = styled('div')(() => ({}))
+
 const ContainePageTwo = styled('div')(() => ({
    width: '55%',
    height: '100%',
 }))
+
 const ContainerTitles = styled('div')(() => ({
    display: 'flex',
    flexDirection: 'column',
@@ -57,4 +82,22 @@ const ContainerTitles = styled('div')(() => ({
          color: 'red',
       },
    },
+}))
+
+const VideoContainer = styled('div')(() => ({
+   position: 'relative',
+   width: '100%',
+   height: '100%',
+}))
+
+const LogoContainer = styled('div')(() => ({
+   position: 'absolute',
+   top: '50%',
+   left: '50%',
+   transform: 'translate(-50%, -50%)',
+}))
+
+const Logo = styled('img')(() => ({
+   width: '45vh',
+   height: '45vh',
 }))
